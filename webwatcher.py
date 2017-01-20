26ac8d0825758d5a49f35a99967ecf9b51e907e2
Alister Bent <abent@college.harvard.edu>
2017-01-20 13:03:57 -0500

    update webwatcher.py

--- webwatcher.py
+++ webwatcher.py
@@ -7,119 +7,100 @@ import requests
 import os
 import datetime
 import threading
+import winsound
 
 def get_nypost(soup):
-    table = soup.find_all('div', {'class': 'site-content box'})[0]
-    article = table.find('article').find('h3')
-    title = article.text
-    link = article.find('a')['href']
-    return title, link
-
-def get_nypost_news(soup):
-    table = soup.find_all('div', {'id': 'primary'})[0]
-    article = table.find('article').find('h3')
-    title = article.text
-    link = article.find('a')['href']
-    return title, link
+    article = soup.find('item')
+    link = article.find('link').text
+    authors = article.find('dc:creator').text.split(', ')
+    if 'Josh Kosman' in authors or 'Claire Atkinson' in authors:
+        return link
+    else:
+        return False
 
 def get_citron(soup):
     article = soup.find('article')
-    title = article.find('h2').text
     story_link = article.find('a')['href']
     link = 'http://citronresearch.com' + story_link
-    return title, link
+    return link
 
 def get_muddy(soup):
     first_cell = soup.find('tbody').find('td')
-    title = first_cell.text
     link = first_cell.find('a')['href']
-    return title, link
+    return link
 
 def get_spruced(soup):
     story = soup.find('h2')
-    title = story.text
     link = story.find('a')['href']
-    return title, link
+    return link
 
 def get_prescience(soup):
     story = soup.find('article').find('h6')
-    title = story.text
     link = story.find('a')['href']
-    return title, link
+    return link
 
 def get_gotham(soup):
     article = soup.find('article')
-    title = article.find('h2').text
     story_link = article.find('a')['href']
     link = 'http://citronresearch.com' + story_link
-    return title, link
+    return link
 
 def get_fdanews(soup):
     table = soup.find_all('div', {'class': 'panel-body'})[0]
     article = table.find('li')
-    title = article.text
     link = article.find('a')['href']
     link = 'http://www.fda.gov' + link
-    return title, link
+    return link
 
 def loop(watcher):
     while True:
-	page = requests.get(watcher['url'])
-	soup = BeautifulSoup(page.text, 'html.parser')
-	title, link = watcher['selector'](soup)
-	if watcher['last_title'] is not None and title != watcher['last_title']:
-	    cmd = 'start "" "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" --new-window "%s"' %link
-	    os.system(cmd)
-	    print str(datetime.datetime.now())
-	watcher['last_title'] = title
-	time.sleep(1)
+        page = requests.get(watcher['url'])
+        soup = BeautifulSoup(page.text, 'html.parser')
+        link = watcher['selector'](soup)
+        if watcher['last_link'] is not None and link != watcher['last_link'] and link:
+            cmd = 'start "" "C:\Program Files (x86)\Google\Chrome\Application\Chrome.exe" --new-window "%s"' %link
+            os.system(cmd)
+            winsound.Beep(440, 500)
+            print str(datetime.datetime.now())
+        watcher['last_link'] = link
+        time.sleep(.5)
 
 
 watchmen = [
     {
-        'url': 'https://www.nypost.com/author/josh-kosman/',
+        'url': 'http://nypost.com/feed/',
         'selector': get_nypost,
-        'last_title': None
-    },
-    {
-        'url': 'https://www.nypost.com/author/claire-atkinson/',
-        'selector': get_nypost,
-        'last_title': None
-    },
-    {
-        'url': 'http://nypost.com/news/',
-        'selector': get_nypost_news,
-        'last_title': None
+        'last_link': None
     },
     {
         'url': 'http://www.citronresearch.com/reports/',
         'selector': get_citron,
-        'last_title': None
+        'last_link': None
     },
     {
         'url': 'http://www.muddywatersresearch.com/research/',
         'selector': get_muddy,
-        'last_title': None
+        'last_link': None
     },
     {
         'url': 'http://www.sprucepointcap.com/research/',
         'selector': get_spruced,
-        'last_title': None
+        'last_link': None
     },
     {
         'url': 'http://www.presciencepoint.com/research/',
         'selector': get_prescience,
-        'last_title': None
+        'last_link': None
     },
     {
         'url': 'https://gothamcityresearch.com/research/',
         'selector': get_gotham,
-        'last_title': None
+        'last_link': None
     },
     {
         'url': 'http://www.fda.gov/NewsEvents/Newsroom/PressAnnouncements/default.htm',
         'selector': get_fdanews,
-        'last_title': None
+        'last_link': None
     }
 ]
 
