c872eaa274c4d4b77657779a705e1a4d0fb1dd04
Alister Bent <abent@college.harvard.edu>
2017-01-26 13:09:02 -0500

    add the street.com

--- webwatcher.py
+++ webwatcher.py
@@ -13,31 +13,15 @@ def get_nypost(soup):
     article = soup.find('item')
     link = article.find('link').text
     authors = article.find('dc:creator').text.split(', ')
-    damelos = ['Josh Kosman', 'Claire Atkinson', 'Associated Press', 'Post Editorial Board']
+    damelos = ['Josh Kosman', 'Claire Atkinson', 'Associated Press']
     if any(damelo in authors for damelo in damelos):
         return link
     else:
         return False
 
-def get_citron(soup):
-    article = soup.find('article')
-    story_link = article.find('a')['href']
-    link = 'http://citronresearch.com' + story_link
-    return link
-
-def get_muddy(soup):
+def get_rss(soup):
     article = soup.find('item')
     link = article.find('link').text
-    return link
-
-def get_spruced(soup):
-    story = soup.find('h2')
-    link = story.find('a')['href']
-    return link
-
-def get_prescience(soup):
-    story = soup.find('article').find('h6')
-    link = story.find('a')['href']
     return link
 
 def get_gotham(soup):
@@ -45,10 +29,15 @@ def get_gotham(soup):
     link = article.find('a')['href']
     return link
 
-def get_fdanews(soup):
-    article = soup.find('item')
-    link = article.find('link').text
-    return link
+def get_street(soup):
+    article = soup.find_all('div', {'class': 'news-list__item'})[0]
+    link = artlcle.find('a')['href']
+    author = article.find_all('div', {'class': 'news-list__author-name'})[0].text
+    damelo = 'Adam Feuerstein'
+    if author == damelo:
+        return link
+    else:
+        return false
 
 def loop(watcher):
     while True:
@@ -75,33 +64,44 @@ watchmen = [
         'last_link': {}
     },
     {
-        'url': 'http://www.citronresearch.com/',
-        'selector': get_citron,
+        'url': 'http://www.citronresearch.com/feed',
+        'selector': get_rss,
         'last_link': {}
     },
     {
         'url': 'http://www.muddywatersresearch.com/feed/?post_type=reports',
-        'selector': get_muddy,
+        'selector': get_rss,
         'last_link': {}
     },
     {
-        'url': 'http://www.sprucepointcap.com/research/',
-        'selector': get_spruced,
+        'url': 'http://www.sprucepointcap.com/research/feed',
+        'selector': get_rss,
         'last_link': {}
     },
     {
-        'url': 'http://www.presciencepoint.com/research/',
-        'selector': get_prescience,
-        'last_link': {}
-    },
-    {
-        'url': 'https://gothamcityresearch.com/research/',
-        'selector': get_gotham,
+        'url': 'http://www.presciencepoint.com/research/feed',
+        'selector': get_rss,
         'last_link': {}
     },
     {
         'url': 'http://www.fda.gov/AboutFDA/ContactFDA/StayInformed/RSSFeeds/PressReleases/rss.xml',
-        'selector': get_fdanews,
+        'selector': get_rss,
+        'last_link': {}
+    },
+    #{
+    #    'url': 'http://apps.shareholder.com/rss/rss.aspx?channels=7196&companyid=ABEA-4CW8X0&sh_auth=3100301180%2E0%2E0%2E42761%2Eb96f9d5de05fc54b98109cd0d905924d',
+    #    'selector': get_rss,
+    #    'last_link': {}
+    #}
+    # Requests.get(thestreet_url) returns response 404!!
+    #{
+    #    'url': 'https://www.thestreet.com',
+    #    'selector': get_street,
+    #    'last_link': {}
+    #},
+    {
+        'url': 'https://gothamcityresearch.com/research/',
+        'selector': get_gotham,
         'last_link': {}
     }
 ]
