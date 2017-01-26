15e4117bd851737c62f214818d7356945907d41f
Alister Bent <abent@college.harvard.edu>
2017-01-26 17:33:05 -0500

    fix thestreet

--- webwatcher.py
+++ webwatcher.py
@@ -31,22 +31,21 @@ def get_gotham(soup):
 
 def get_street(soup):
     article = soup.find_all('div', {'class': 'news-list__item'})[0]
-    link = article.find('a')['href']
+    link = artlcle.find('a')['href']
     author = article.find_all('div', {'class': 'news-list__author-name'})[0].text
     damelo = 'Adam Feuerstein'
     if author == damelo:
         return link
     else:
-        return False
+        return false
 
 def loop(watcher):
     while True:
         try:
-	    headers = {
+            headers = {
 		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'
 	    }
-
-            page = requests.get(watcher['url'], headers=headers)
+            page = requests.get(watcher['url'])
             soup = BeautifulSoup(page.text, 'html.parser')
             link = watcher['selector'](soup)
         except Exception as e:
@@ -65,43 +64,51 @@ watchmen = [
     {
         'url': 'http://nypost.com/feed/',
         'selector': get_nypost,
+        'last_link': {}
     },
     {
         'url': 'http://www.citronresearch.com/feed',
         'selector': get_rss,
+        'last_link': {}
     },
     {
         'url': 'http://www.muddywatersresearch.com/feed/?post_type=reports',
         'selector': get_rss,
+        'last_link': {}
     },
     {
         'url': 'http://www.sprucepointcap.com/research/feed',
         'selector': get_rss,
+        'last_link': {}
     },
     {
         'url': 'http://www.presciencepoint.com/research/feed',
         'selector': get_rss,
+        'last_link': {}
     },
     {
         'url': 'http://www.fda.gov/AboutFDA/ContactFDA/StayInformed/RSSFeeds/PressReleases/rss.xml',
         'selector': get_rss,
+        'last_link': {}
     },
     #{
     #    'url': 'http://apps.shareholder.com/rss/rss.aspx?channels=7196&companyid=ABEA-4CW8X0&sh_auth=3100301180%2E0%2E0%2E42761%2Eb96f9d5de05fc54b98109cd0d905924d',
     #    'selector': get_rss,
-    #}
+    #    'last_link': {}
+    #},
     {
         'url': 'https://www.thestreet.com',
         'selector': get_street,
+        'last_link': {}
     },
     {
         'url': 'https://gothamcityresearch.com/research/',
         'selector': get_gotham,
+        'last_link': {}
     }
 ]
 
 for watcher in watchmen:
-    watcher['last_link'] = {}
     t = threading.Thread(target=loop, args=(watcher,))
     t.daemon = True
     t.start()
